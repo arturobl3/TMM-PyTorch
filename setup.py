fc46eebc3218ef6325d338a1c99614bc91844d90
RodionovSA <sergeyrodionov93@gmail.com>
2025-07-22 16:00:19 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="TMM-PyTorch",
+    version="0.1",
+    packages=find_packages(),  # auto-discovers Python modules
+    install_requires=[
+        
+    ],
+)
