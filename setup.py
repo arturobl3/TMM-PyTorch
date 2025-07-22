d2397f2d5a9b4998cd1179b93048abd3cf7126ad
RodionovSA <sergeyrodionov93@gmail.com>
2025-07-22 16:22:43 +0300

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ setup(
     name="TMM-PyTorch",
     version="0.1",
     packages=find_packages(),
-    python_requires='>=3.9',  
+    python_requires='>=3.10',  
     install_requires=[
         "torch>=2.0",
+        "numpy>=1.21",
     ],
 )
