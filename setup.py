5cb73939e64f83b1fcd8ec707289ea6cb27ce1ed
RodionovSA <sergeyrodionov93@gmail.com>
2025-07-22 16:09:41 +0300

    updated setup

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(),  # auto-discovers Python modules
     install_requires=[
-        
+        "python>=3.9",  # Python version requirement
+        "torch>=2.0",  # PyTorch version requirement
     ],
 )
